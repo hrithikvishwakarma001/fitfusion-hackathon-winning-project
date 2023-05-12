3323a47eba4415b8fe89d83f85ab6ae9799870e5
Hrithik Vishwakarma <hritik8008@gmail.com>
2023-05-12 16:43:56 +0530

    google authentication completed

---
+++ backend/confgs/google.js
@@ -0,0 +1,34 @@
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const passport = require("passport");
+require('dotenv').config()
+
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
+      callbackURL: "http://localhost:4000/auth/google/callback",
+      scope: ["profile", "email"],
+    },
+    function (accessToken, refreshToken, profile, done) {
+      // console.log(profile)//!--> consoling profile;
+      done(null, profile);
+    }
+  )
+);
+
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
+
+module.exports = passport;
