119b010847d9a478420a84990cba0a2be9380d69
Hrithik Vishwakarma <hritik8008@gmail.com>
2023-05-12 17:24:40 +0530

    add sidebar properly

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,14 @@
+import { useLocation } from "react-router-dom";
+import Home from "./pages/Home";
 import Router from "./routes/Router";
 
 function App() {
+	const location = useLocation();
+	const pathName = location.pathname;
 	return (
 		<div className='bg-slate-200 max-h-screen'>
-			<Router />
+			{(pathName === "/" || pathName === "/login") && <Router />}
+			{pathName !== "/" && pathName !== "/login" && <Home />}
 		</div>
 	);
 }
