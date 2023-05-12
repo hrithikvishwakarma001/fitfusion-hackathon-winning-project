99c224305fe0250d7467f4431077457ffd963bda
Hrithik Vishwakarma <hritik8008@gmail.com>
2023-05-12 09:51:09 +0530

    add sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import SideBar from "./components/SideBar";
+import Router from "./routes/Router";
 
 function App() {
 	return (
 		<div className='bg-slate-200 max-h-400px'>
-			<SideBar />
+			{/* <SideBar /> */}
+			<Router/>
 		</div>
 	);
 }
