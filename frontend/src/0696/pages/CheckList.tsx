59e29259cb5fd63373318bb5db87ffad141873ee
Hrithik Vishwakarma <hritik8008@gmail.com>
2023-05-14 11:14:25 +0530

    some bug fixed

--- frontend/src/0696/pages/CheckList.tsx
+++ frontend/src/0696/pages/CheckList.tsx
@@ -1,21 +1,11 @@
 import React from "react";
-import { ChakraProvider, ColorModeScript, extendTheme } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 import Todo from "./Todo";
 
 export default function Checklist() {
-  const config = {
-    initialColorMode: "dark",
-    useSystemColorMode: false,
-  };
-
-  const theme = extendTheme({
-    config,
-  });
-
-  return (
-    <ChakraProvider theme={theme}>
-      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      <Todo />
-    </ChakraProvider>
-  );
+	return (
+		<ChakraProvider>
+			<Todo />
+		</ChakraProvider>
+	);
 }
